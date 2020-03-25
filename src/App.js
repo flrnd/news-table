57e7eaf310e264e6363a1fc4abfe983bb1efb0ca
Florian Rand <florian.rand@gmail.com>
2020-03-25 15:14:54 +0100

    minimal table render

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
+const header = [" ", "DATE", "HEADING", "URL"];
 function App() {
+  const [data, setData] = useState({ articles: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const KEY = process.env.REACT_APP_API_KEY;
+      const result = await axios(
+        `http://newsapi.org/v2/everything?q=corona&from=2020-03-24&to=2020-03-28&apiKey=${KEY}`
+      );
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              {header.map((heading, index) => (
+                <th key={index}>{heading}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.articles.map((article, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{article.publishedAt.split("T")[0]}</td>
+                <td>{article.title}</td>
+                <td>{article.url}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/components/cell.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Cell = props => (
+  <td background={props.background}>{props.content}</td>
+);

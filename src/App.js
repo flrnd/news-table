2e7d5c113eb6f17c35289283eb371d7d0e5fcec6
Florian Rand <florian.rand@gmail.com>
2020-03-25 16:22:38 +0100

    Add search

--- src/App.js
+++ src/App.js
@@ -2,33 +2,51 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-const header = [" ", "DATE", "HEADING", "URL"];
+const firstRow = [" ", "A", "B", "C"];
+const header = [" ", "Date", "Header", "Link"];
 function App() {
   const [data, setData] = useState({ articles: [] });
+  const [query, setQuery] = useState("apple");
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_URL = `http://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`;
+  const [url, setUrl] = useState(API_URL);
 
   useEffect(() => {
     const fetchData = async () => {
-      const KEY = process.env.REACT_APP_API_KEY;
-      const result = await axios(
-        `http://newsapi.org/v2/everything?q=corona&from=2020-03-24&to=2020-03-28&apiKey=${KEY}`
-      );
+      const result = await axios(url);
       setData(result.data);
     };
     fetchData();
-  }, []);
+  }, [url]);
 
   return (
     <div className="App">
+      <div className="search">
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="button" onClick={() => setUrl(API_URL)}>
+          Search
+        </button>
+      </div>
       <div className="table-wrapper">
         <table>
           <thead>
             <tr>
-              {header.map((heading, index) => (
+              {firstRow.map((heading, index) => (
                 <th key={index}>{heading}</th>
               ))}
             </tr>
           </thead>
           <tbody>
+            <tr>
+              {header.map((heading, index) => (
+                <td key={index}>{heading}</td>
+              ))}
+            </tr>
+
             {data.articles.map((article, index) => (
               <tr key={index}>
                 <td>{index + 1}</td>

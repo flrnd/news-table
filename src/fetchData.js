1d61ca168510d441dd7820aba275d3a93b11d461
Florian Rand <florian.rand@gmail.com>
2020-03-26 21:48:01 +0100

    move constants outside and return directly

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const fetchData = async (query) => {
-  const API_KEY = process.env.REACT_APP_API_KEY;
-  const API_URL = `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`;
-
+const API = "https://newsapi.org/v2";
+const API_KEY = process.env.REACT_APP_API_KEY;
+export const fetchData = async (query) => {
+  const url = `${API}/everything?q=${query}&apiKey=${API_KEY}`;
   try {
-    const result = await axios(API_URL);
-    return result.data;
+    return await axios.get(url);
   } catch (error) {
     console.error(`Fetch error: ${error}`);
   }

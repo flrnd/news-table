95ab9adc7ccd140c9647b0dae2ef99355baf827f
Florian Rand <florian.rand@gmail.com>
2020-03-26 21:48:14 +0100

    fetchData test

---
+++ src/fetchData.test.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import fetchData from "./fetchData";
+
+jest.mock("axios");
+
+describe("fetchData", () => {
+  it("should fetch articles from API", async () => {
+    const resp = {
+      data: {
+        status: "ok",
+        totalResults: 2072,
+        articles: [
+          {
+            publishedAt: "2020-03-24T15:07:00Z",
+            title:
+              "Kingdom of Frost Explores How Frozen Landscapes Shaped Humanity",
+            url:
+              "https://earther.gizmodo.com/kingdom-of-frost-explores-how-fro",
+          },
+          {
+            publishedAt: "2020-02-28T02:30:00Z",
+            title: "Oh Hell Yeah More Isometric Landscapes",
+            url: "https://kotaku.com/oh-hell-yeah-more-isometric-landscapes",
+          },
+        ],
+      },
+    };
+    axios.get.mockImplementationOnce(() => Promise.resolve(resp));
+    await expect(fetchData("query")).resolves.toEqual(resp);
+  });
+});

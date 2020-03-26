220a5458f80d322da543c0f1380b35812d655cdc
Florian Rand <florian.rand@gmail.com>
2020-03-26 22:51:23 +0100

    Add listener for Enter on input

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from "react";
-import fetchData from "./fetchData";
+import { fetchData, API, API_KEY } from "./fetchData";
 import Table from "./components/Table";
 import "./App.css";
 
 const header = [" ", "Date", "Header", "Link"];
+
 function App() {
   const [data, setData] = useState({ articles: [] });
   const [query, setQuery] = useState("landscapes");
+  const API_URL = `${API}/everything?q=${query}&apiKey=${API_KEY}`;
+  const [url, setUrl] = useState(API_URL);
 
   useEffect(() => {
     fetchData(query)
       .then((response) => setData(response.data))
       .catch((error) => console.log(error));
-  }, [query]);
+  }, [url]);
 
   return (
     <div className="App">
@@ -21,18 +24,16 @@ function App() {
           type="text"
           value={query}
           onChange={(event) => setQuery(event.target.value)}
+          onKeyPress={(event) =>
+            event.key === "Enter" ? setUrl(API_URL) : null
+          }
         />
-        <button type="button" onClick={() => setQuery(query)}>
+        <button type="button" onClick={() => setUrl(API_URL)}>
           Search
         </button>
       </div>
       <div className="table-wrapper">
-        <Table
-          id="data"
-          header={header}
-          body={data.articles}
-          columnSize={3}
-        ></Table>
+        <Table header={header} body={data.articles} columnSize={3} />
       </div>
     </div>
   );

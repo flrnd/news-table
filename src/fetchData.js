2fea732fa0d2c7d9a1e82c836764dbc74ecfd5a3
Florian Rand <florian.rand@gmail.com>
2020-03-28 13:35:29 +0100

    Remove pagination :P

--- src/fetchData.js
+++ src/fetchData.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const API = "https://newsapi.org/v2";
 export const API_KEY = process.env.REACT_APP_API_KEY;
 export const fetchData = async (query, page) => {
-  const url = `${API}/everything?page=${page}&q=${query}&sortBy=publishedAt&apiKey=${API_KEY}`;
+  const url = `${API}/everything?q=${query}&sortBy=publishedAt&apiKey=${API_KEY}`;
   try {
     return await axios.get(url);
   } catch (error) {

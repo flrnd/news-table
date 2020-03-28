32db39409790a7498f9f0c422efb80dff83eb19a
Florian Rand <florian.rand@gmail.com>
2020-03-28 15:47:11 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import SearchPage from "./page/SearchPage";
+import HistoryPage from "./page/HistoryPage";
+import Navigation from "./components/Navigation";
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -11,25 +15,31 @@ const NoMatchRoute = () => <div>404 Page</div>;
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <Row>
-          <Col>
-            <Router>
-              <Switch>
-                <Route path="/" exact component={SearchPage} />
-                <Route component={NoMatchRoute} />
-              </Switch>
-            </Router>
-          </Col>
-        </Row>
-      </Container>
-      <footer>
-        <div style={{ paddingTop: "20px" }}>
-          <a href="https://newsapi.org/">Powered By NewsAPI.org</a>
-        </div>
-      </footer>
-    </div>
+    <Container className="App">
+      <Row>
+        <Col>
+          <Navigation />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Router>
+            <Switch>
+              <Route path="/" exact component={SearchPage} />
+              <Route path="/history" exact component={HistoryPage} />
+              <Route component={NoMatchRoute} />
+            </Switch>
+          </Router>
+        </Col>
+      </Row>
+      <Row>
+        <footer>
+          <div style={{ paddingTop: "20px" }}>
+            <a href="https://newsapi.org/">Powered By NewsAPI.org</a>
+          </div>
+        </footer>
+      </Row>
+    </Container>
   );
 }
 

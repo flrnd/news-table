d8dd5cf288b744b4b776424fb75f177f8091c960
Florian Rand <florian.rand@gmail.com>
2020-03-28 15:14:11 +0100

    separate page for searchs, using react router

--- src/App.js
+++ src/App.js
@@ -1,67 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SearchPage from "./page/SearchPage";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { fetchData, normalizeData, API, API_KEY } from "./fetchData";
-import { saveStore } from "./store";
-import NewsTable from "./components/Table";
-import SearchForm from "./components/SearchForm";
+
 import "./App.css";
 
+const NoMatchRoute = () => <div>404 Page</div>;
+
 function App() {
-  const [data, setData] = useState({ articles: [] });
-  const [query, setQuery] = useState("");
-  const [browseHistory, setBrowseHistory] = useState({ articles: [] });
-  const API_URL = `${API}/everything?q=${query}&sortBy=publishedAt&apiKey=${API_KEY}`;
-  const [url, setUrl] = useState(API_URL);
-
-  useEffect(() => {
-    fetchData(query)
-      .then((response) => {
-        const normalized = normalizeData(response.data);
-        setData(normalized.data);
-        setBrowseHistory(normalized.data);
-      })
-      .catch((error) => console.log(error));
-    // eslint-disable-next-line
-  }, [url]);
-
-  const onSearchHandler = () => {
-    setUrl(API_URL);
-    saveStore(browseHistory.articles);
-  };
-
-  const onInputChange = (event) => {
-    setQuery(event.target.value);
-  };
-
   return (
     <div className="App">
       <Container>
         <Row>
           <Col>
-            <SearchForm
-              onSearchHandler={onSearchHandler}
-              onInputChange={onInputChange}
-            />
-          </Col>
-          <Col xs lg="2"></Col>
-        </Row>
-        <Row>
-          <Col>
-            <div className="table-wrapper">
-              <NewsTable body={data.articles} columnSize={3} />
-            </div>
-            <div style={{ paddingTop: "20px" }}>
-              <a href="https://newsapi.org/">Powered By NewsAPI.org</a>
-            </div>
-          </Col>
-          <Col xs lg="2">
-            <h2>History</h2>
-            <span>items saved</span>
+            <Router>
+              <Switch>
+                <Route path="/" exact component={SearchPage} />
+                <Route component={NoMatchRoute} />
+              </Switch>
+            </Router>
           </Col>
         </Row>
       </Container>
+      <footer>
+        <div style={{ paddingTop: "20px" }}>
+          <a href="https://newsapi.org/">Powered By NewsAPI.org</a>
+        </div>
+      </footer>
     </div>
   );
 }

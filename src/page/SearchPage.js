342d36c9e3b9b1aa6ca43b5b3b694b4d36e2dc84
Florian Rand <florian.rand@gmail.com>
2020-03-28 15:13:56 +0100

    separate page for search

---
+++ src/page/SearchPage.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { fetchData, normalizeData, API, API_KEY } from "../fetchData";
+import { saveStore } from "../store";
+import NewsTable from "../components/Table";
+import SearchForm from "../components/SearchForm";
+import "./SearchPage.css";
+
+function SearchPage() {
+  const [data, setData] = useState({ articles: [] });
+  const [query, setQuery] = useState("");
+  const [browseHistory, setBrowseHistory] = useState({ articles: [] });
+  const API_URL = `${API}/everything?q=${query}&sortBy=publishedAt&apiKey=${API_KEY}`;
+  const [url, setUrl] = useState(API_URL);
+
+  useEffect(() => {
+    if (query)
+      fetchData(query)
+        .then((response) => {
+          const normalized = normalizeData(response.data);
+          setData(normalized.data);
+          setBrowseHistory(normalized.data);
+        })
+        .catch((error) => console.log(error));
+    // eslint-disable-next-line
+  }, [url]);
+
+  const onSearchHandler = () => {
+    setUrl(API_URL);
+    saveStore(browseHistory.articles);
+  };
+
+  const onInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  return (
+    <>
+      <Row>
+        <Col>
+          <SearchForm
+            onSearchHandler={onSearchHandler}
+            onInputChange={onInputChange}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="table-wrapper">
+            <NewsTable body={data.articles} columnSize={3} />
+          </div>
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+export default SearchPage;

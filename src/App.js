73f7267efadf7e9328adcabf0207292c7de78066
Florian Rand <florian.rand@gmail.com>
2020-03-28 14:28:42 +0100

    Move code to SearchForm component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { fetchData, normalizeData, API, API_KEY } from "./fetchData";
-import { saveStore, getStore, TODAY } from "./store";
+import { saveStore } from "./store";
 import NewsTable from "./components/Table";
+import SearchForm from "./components/SearchForm";
 import "./App.css";
-
-const header = [" ", "Date", "Header", "Link"];
-const totalSavedHistory = async (day) => {
-  const saved = await getStore(day);
-  return saved.length;
-};
 
 function App() {
   const [data, setData] = useState({ articles: [] });
-  const [query, setQuery] = useState("landscapes");
+  const [query, setQuery] = useState("");
   const [browseHistory, setBrowseHistory] = useState({ articles: [] });
   const API_URL = `${API}/everything?q=${query}&sortBy=publishedAt&apiKey=${API_KEY}`;
   const [url, setUrl] = useState(API_URL);
@@ -33,9 +26,13 @@ function App() {
     // eslint-disable-next-line
   }, [url]);
 
-  const onSearchHandle = () => {
+  const onSearchHandler = () => {
     setUrl(API_URL);
     saveStore(browseHistory.articles);
+  };
+
+  const onInputChange = (event) => {
+    setQuery(event.target.value);
   };
 
   return (
@@ -43,33 +40,17 @@ function App() {
       <Container>
         <Row>
           <Col>
-            <Navbar expand="sm">
-              <Navbar.Brand href="#home">News search</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <div>
-                  <input
-                    type="text"
-                    placeholder="Search"
-                    className="mr-sm-2"
-                    onChange={(event) => setQuery(event.target.value)}
-                    onKeyPress={(event) =>
-                      event.key === "Enter" ? onSearchHandle() : null
-                    }
-                  />
-                  <Button variant="outline-success" onClick={onSearchHandle}>
-                    Search
-                  </Button>
-                </div>
-              </Navbar.Collapse>
-            </Navbar>
+            <SearchForm
+              onSearchHandler={onSearchHandler}
+              onInputChange={onInputChange}
+            />
           </Col>
           <Col xs lg="2"></Col>
         </Row>
         <Row>
           <Col>
             <div className="table-wrapper">
-              <NewsTable header={header} body={data.articles} columnSize={3} />
+              <NewsTable body={data.articles} columnSize={3} />
             </div>
             <div style={{ paddingTop: "20px" }}>
               <a href="https://newsapi.org/">Powered By NewsAPI.org</a>

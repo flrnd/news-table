58b37891fd82fdd4f2bf325bb0baa57cad4a2ce4
Florian Rand <florian.rand@gmail.com>
2020-03-29 17:14:21 +0200

    formating and datepickers

--- src/page/HistoryPage.js
+++ src/page/HistoryPage.js
@@ -1,32 +1,68 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ListGroup from "react-bootstrap/ListGroup";
+
 import { getAllStored } from "../store";
 import { getItemTime, getDateWithNames } from "../util/date";
-import Button from "react-bootstrap/Button";
+
+import "react-datepicker/dist/react-datepicker.css";
+import "./HistoryPage.css";
+
 const TODAY = Date.now();
+
 function HistoryPage() {
   const [history, setHistory] = useState({ data: [] });
+  const [startDate, setStartDate] = useState(TODAY);
+  const [endDate, setEndDate] = useState(TODAY);
 
-  const fetchHistory = () => {
-    getAllStored().then((item) => {
-      setHistory({ data: item });
+  const fetchHistory = (start, end) => {
+    getAllStored(start, end).then((stored) => {
+      setHistory({ data: stored });
     });
   };
 
+  const handleStartDateChange = (date) => {
+    setStartDate(date.getTime());
+  };
+
+  const handleEndDateChange = (date) => {
+    setEndDate(date.getTime());
+  };
+
+  useEffect(() => {
+    if (startDate && endDate) fetchHistory(startDate, endDate);
+  }, [startDate, endDate]);
   return (
     <>
-      <Button onClick={fetchHistory}>fetch</Button>
-      <div style={{ paddingTop: "25px" }}>
-        <h2>Today - {getDateWithNames(TODAY)}</h2>
-      </div>
-      <div style={{ paddingTop: "15px" }}>
-        <ul>
-          {history.data.map((h, i) => (
-            <li key={h.key} id={h.key}>
-              {getItemTime(h.key)} {h.value}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Row>
+        <Col sm={4}>
+          <div className="label">Star date</div>
+          <DatePicker selected={startDate} onChange={handleStartDateChange} />
+        </Col>
+        <Col sm={4}>
+          <div className="label">End date</div>
+          <DatePicker selected={endDate} onChange={handleEndDateChange} />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="date-picker-list">
+            <h2>{getDateWithNames(startDate)}</h2>
+            <ListGroup>
+              {history.data.map((h, i) => (
+                <ListGroup horizontal="sm">
+                  <ListGroup.Item key={h.key} id={h.key}>
+                    {getItemTime(h.key)}
+                  </ListGroup.Item>
+                  <ListGroup.Item>{h.value}</ListGroup.Item>
+                </ListGroup>
+              ))}
+            </ListGroup>
+          </div>
+        </Col>
+      </Row>
     </>
   );
 }
